c03fe3feeb21fa4cc3075869a7cc099c68cdc6b1
Precious <preciousmaregwa1997@gmail.com>
2022-06-12 13:01:15 +0300

    completed assigment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,19 @@
-
+class Book
+    attr_reader :title
+    attr_accessor :author, :page_count, :genre
+  
+    def initialize(title, author="Agatha Christie", page_count=272, genre="Mystery")
+      @title = title
+      @author = author
+      @page_count = page_count
+      @genre = genre
+    end
+  
+    def turn_page
+      @title
+      puts "Flipping the page...wow, you read fast!"
+    end
+  end
+  
+  book = Book.new("And Then There Were None")
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,18 @@
 # Make your shoe class here!
+class Shoe
+    attr_reader :brand
+    attr_accessor :color, :size, :material, :condition
+    def initialize(brand, color = "black", size = 6, material = "suede", condition = "tattered")
+      @brand = brand
+      @color = color
+      @size = size
+      @material = material
+    end
+  
+    def cobble
+      puts "Your shoe is as good as new!"
+      @condition = "new"
+    end
+  end
+  
+  shoe = Shoe.new("Adidas")
